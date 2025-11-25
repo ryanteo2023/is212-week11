9ffe95752200b9e2cb7e10f93af432062dcbc3a3
ryanteo2023 <ryan.teo.2023@smu.edu.sg>
2025-11-25 12:06:10 +0800

    Update ex3_smelly.py

--- ex3/ex3_smelly.py
+++ ex3/ex3_smelly.py
@@ -25,5 +25,5 @@ class Clothing(Product):
 
 class Grocery(Product):
     def __init__(self, name, price):
-        super().__init__(name, price, discount_rate=0.05, tax_rate=0.01)
+        super().__init__(name, price, discount_rate=0.05, tax_rate=0.02)
 
